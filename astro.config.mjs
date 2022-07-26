a26b45106b5d1b1e594e1269963f672e9cf8d13e
MoofyWoofy <59331821+MoofyWoofy@users.noreply.github.com>
2022-07-26 12:52:31 +0800

    fixed search

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,4 +14,5 @@ export default defineConfig({
       wrap:true,
     },
   },
+  trailingSlash:'always',
 });
--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,7 +1,7 @@
 // File routes export a get() function, which gets called to generate the file.
 // Return an object with `body` to save the file contents in your final build.
 export async function get() {
-  const allPosts = import.meta.globEager("./blog/posts/*.md"); // Vite
+  const allPosts = import.meta.globEager("./blog/articles/*.md"); // Vite
   const posts = Object.values(allPosts)
     .filter((ele) => ele.frontmatter.draft != true)
     .map((ele) => {
