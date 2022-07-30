b167872566593c76f249129b09eaddd11a062df8
MoofyWoofy <59331821+MoofyWoofy@users.noreply.github.com>
2022-07-30 15:47:33 +0800

    fixed issues and added accessibility

--- src/config.js
+++ src/config.js
@@ -39,6 +39,7 @@ export default {
     {
       link: "https://www.youtube.com/",
       svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="#c4302b"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>',
+      sr: "link to my YouTube channel",
     },
     {
       link: "https://www.twitter.com/",
@@ -58,9 +59,9 @@ export default {
     },
   ],
   metaData: {
-    authorName: "Melvin",
+    authorName: "Bob",
     summary: "My personal blog",
     category: "Blog",
-    keywords: ["Blog", "Guide"],
+    keywords: ["Blog", "Astro"],
   },
 };
