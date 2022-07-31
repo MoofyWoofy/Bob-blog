bdd9f049e7c4dd13d87c2546e6f4569622d411ce
MoofyWoofy <59331821+MoofyWoofy@users.noreply.github.com>
2022-07-31 12:51:52 +0800

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: "https://moofywoofy.github.io",
-  base: "/Astro-blog",
-  markdown:{
-    shikiConfig:{
-      theme:"one-dark-pro",
+  base: "/Bob-blog",
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
       // Enable word wrap to prevent horizontal scrolling
-      wrap:true,
+      wrap: true,
     },
   },
-  trailingSlash:'always',
+  trailingSlash: "always",
 });
